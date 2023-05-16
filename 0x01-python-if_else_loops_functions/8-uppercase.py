3f9f3d304c26dfede6dea27e7e2bb0d923e03c29
Yassein Idris <yasseinidris@Yasseins-MacBook-Air.local>
2023-05-17 01:37:08 +0300

    up

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -2,6 +2,6 @@
 def uppercase(str):
     for i in str:
         if ord(i) > 96 and ord(i) < 197:
-            print(i.upper(), end="")
+            print("{}".format(i.upper()), end="")
         else:
-            print(i, end="")
+            print("{}".format(i), end="")
