392bac82d80a5a93f6093136bc18410edd088051
Yassein Idris <yasseinidris@Yasseins-MacBook-Air.local>
2023-05-16 11:40:10 +0300

    up

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+
+/**
+ * check_cycle - check the code
+ * @list: linked list to check
+ * Return: 1 if the list has a cycle, 0 if it doesn't
+ */
+int check_cycle(listint_t *list)
+{
+	listint_t *current = list;
+	listint_t *after;
+
+	if (!list)
+		return (0);
+	after = list->next;
+
+	while (current && after)
+	{
+		current = current->next;
+		after = after->next;
+		if (current == after)
+			return (1);
+	}
+
+	return (0);
+}
