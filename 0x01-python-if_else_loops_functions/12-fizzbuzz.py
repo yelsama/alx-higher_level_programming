cc7f530123078d720ac879639583774816f53ada
Yassein Eltayeb Mohamed Idris <ymohamed@lab1r4s13.42abudhabi.ae>
2023-05-19 15:28:08 +0400

    up

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def fizzbuzz():
-	for i in range(1, 100):
+	for i in range(1, 101):
 		if i % 3 == 0 and i % 5 == 0:
-			print("Fizz Buzz ", end="")
+			print("FizzBuzz ", end="")
 		elif i % 3 == 0:
 			print("Fizz ", end="")
 		elif i % 5 == 0:
