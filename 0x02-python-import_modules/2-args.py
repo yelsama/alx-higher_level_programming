f060e0a7979c7fd02c20892292d529139d929720
Yassein Eltayeb Mohamed Idris <ymohamed@lab1r4s13.42abudhabi.ae>
2023-05-20 17:51:52 +0400

    up

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import sys
-    args = len(sys.argv) - 1
-    if count == 1:
-	    print("1 argument:")
-    else:
-	    print("{} arguments.".format(args))
-    for i in range(1, args + 1):
-	    print("{}: {}".format(i, sys.argv[i]))
+arg = len(sys.argv) - 1
+if arg == 0:
+	print("0 arguments.")
+elif arg == 1:
+	print("1 argument:")
+else:
+	print("{} arguments:".format(arg))
+for i in range(arg):
+	print("{}: {}".format(i + 1, sys.argv[i + 1]))
