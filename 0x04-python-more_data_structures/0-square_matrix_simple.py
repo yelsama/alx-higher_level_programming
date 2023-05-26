3517a383242c964bbd6a2b1feeff27affab43441
Yassein Eltayeb Mohamed Idris <ymohamed@lab1r4s13.42abudhabi.ae>
2023-05-26 20:13:55 +0400

    up

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -2,8 +2,5 @@
 def square_matrix_simple(matrix=[]):
     if not matrix:
         return None
-    resl = [[0 for i in range(3)] for i in range(3)]
-    for i in range(0, len(matrix)):
-        for j in range(0, len(matrix[i])):
-            resl[i][j] = matrix[i][j] ** 2
+    resl = [[y ** 2 for y in x] for x in matrix]
     return resl
