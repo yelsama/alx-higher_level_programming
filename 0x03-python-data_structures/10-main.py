3ff7a5b7bf7e056a2dd3828ef147d0eb9c606c9d
Yassein Eltayeb Mohamed Idris <ymohamed@lab1r4s13.42abudhabi.ae>
2023-05-26 15:40:16 +0400

    up

---
+++ 0x03-python-data_structures/10-main.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+get_it = __import__('10-divisible_by_2').divisible_by_2
+
+my_list = [0, 1, 2, 3, 4, 5, 6]
+list_result = get_it(my_list)
+
+i = 0
+while i < len(list_result):
+    print("{:d} {:s} divisible by 2".format(my_list[i], "is" if list_result[i] else "is not"))
+    i += 1
