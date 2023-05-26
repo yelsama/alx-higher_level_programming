4a0f1be43920e032b08658900411d640b58c5ded
Yassein Eltayeb Mohamed Idris <ymohamed@lab1r4s13.42abudhabi.ae>
2023-05-26 22:05:10 +0400

    up

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -2,4 +2,4 @@
 def print_sorted_dictionary(a_dictionary):
     new = sorted(a_dictionary)
     for x in new:
-        print("{}: {}".format(x, a_dictionary(x)))
+        print("{}: {}".format(x, a_dictionary[x]))
