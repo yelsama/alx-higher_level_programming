780b8186e30181e707cc5b80ba6b6b9c1e6f1f27
Yassein Eltayeb Mohamed Idris <ymohamed@lab1r4s11.42abudhabi.ae>
2023-05-29 14:57:23 +0400

    up

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+    n = 0
+    try:
+        for i in range(x):
+            print(my_list[i], end="")
+            n += 1
+    except IndexError:
+        pass
+    print()
+    return n
