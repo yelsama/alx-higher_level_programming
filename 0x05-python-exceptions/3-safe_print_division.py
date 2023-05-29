4e68bb0abf3507b3a62c1dd2528c66ecd5906caa
Yassein Eltayeb Mohamed Idris <ymohamed@lab1r4s11.42abudhabi.ae>
2023-05-29 19:58:09 +0400

    up

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -5,5 +5,6 @@ def safe_print_division(a, b):
         result = a / b
     except ZeroDivisionError:
         pass
-    print("Inside result: {}".format(result))
+    finally:
+        print("Inside result: {}".format(result))
     return result
