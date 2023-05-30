d63e67cccda3c0a51c2549e8d6a7b810020dd780
Yassein Eltayeb Mohamed Idris <ymohamed@lab1r4s11.42abudhabi.ae>
2023-05-30 17:02:27 +0400

    up

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python3
+"""
+This class represents a square shape.
+Attributes:
+    None
+Methods:
+    area
+    print
+"""
+
+
+class Square:
+    """
+    This class represents a square shape.
+    Attributes:
+        None
+    Methods:
+        area
+        print
+    """
+    def __init__(self, size=0):
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        return self.size ** 2
+
+    def my_print(self):
+        for i in range(self.size):
+            for j in range(self.size):
+                print("#", end="")
+            print()   
