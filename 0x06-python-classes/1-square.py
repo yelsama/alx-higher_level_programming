23f3e2d9b10e7f5d5dad9f0585751eaeb0cbb3e6
Yassein Eltayeb Mohamed Idris <ymohamed@lab1r4s11.42abudhabi.ae>
2023-05-30 15:23:51 +0400

    up

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+#!/usr/bin/python3
+"""
+This class.
+Attributes:
+    None
+Methods:
+    None
+"""
+
+
+class Square:
+    """
+    This class represents a square shape.
+    Attributes:
+        None
+    Methods:
+        None
+    """
+    def __init__(self, size = None):
+            self.__size = size
