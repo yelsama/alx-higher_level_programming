c3a6a35feceb725b676934eee7c2634ca3b54ac9
Yassein Eltayeb Mohamed Idris <ymohamed@lab1r4s11.42abudhabi.ae>
2023-05-30 15:57:26 +0400

    up

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+This class represents a square shape.
+Attributes:
+    area
+Methods:
+    None
+"""
+
+
+class Square:
+    """
+    This class represents a square shape.
+    Attributes:
+        None
+    Methods:
+        area
+    """
+    def __init__(self, size=0):
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+    
+    def area(self):
+            return self.__size ** 2
