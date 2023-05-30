f7be665c89333d1b620cd7da47ff2b1527008661
Yassein Eltayeb Mohamed Idris <ymohamed@lab1r4s11.42abudhabi.ae>
2023-05-30 13:48:22 +0400

    up

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,12 +1,14 @@
 #!/usr/bin/python3
+
+"""
+This class represents a square shape.
+
+Attributes:
+	None
+
+Methods:
+	None
+"""
 class Square:
-    """
-    This class represents a square shape.
 
-    Attributes:
-        None
-
-    Methods:
-        None
-    """
     pass
