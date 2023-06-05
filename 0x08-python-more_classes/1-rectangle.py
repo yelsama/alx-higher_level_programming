47522c6e964d9846b76847d64de5350e60f3ed7d
Yassein Eltayeb Mohamed Idris <ymohamed@lab1r4s11.42abudhabi.ae>
2023-06-05 22:04:55 +0400

    up

---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+This class represents a rectanglar shape.
+Attributes:
+    None
+Methods:
+    None
+"""
+
+
+class Rectangle:
+    """
+    This class represents a rectanglar shape.
+    Attributes:
+        None
+    Methods:
+        None
+    """
+    def __init__(self, he)
