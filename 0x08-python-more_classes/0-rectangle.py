28c548b03c6b1cfc53a9db0449a3dc87b477fcf3
Yassein Eltayeb Mohamed Idris <ymohamed@lab1r4s11.42abudhabi.ae>
2023-06-05 21:52:10 +0400

    up

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+This class represents a square shape.
+Attributes:
+    None
+Methods:
+    None
+"""
+
+class Rectangle:
+    """
+    This class represents a square shape.
+    Attributes:
+        None
+    Methods:
+        None
+    """
+    pass
