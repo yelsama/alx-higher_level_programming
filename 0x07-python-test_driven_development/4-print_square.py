53995405bef8b5b799a200a34b19b288b1792107
Yassein Eltayeb Mohamed Idris <ymohamed@lab1r4s11.42abudhabi.ae>
2023-06-06 12:03:00 +0400

    up

---
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+def print_square(size):
+    """prints a square out of hashs"""
+    if not isinstance(size, int):
+        raise TypeError("size must be an integer")
+    if size < 0:
+        raise ValueError("size must be >= 0")
+    for i in range(size):
+        for j in range(size):
+            print("#", end="")
+        print()
