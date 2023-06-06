61e1932cfe60f43fe8ca7206235e3fda65c57db4
Yassein Eltayeb Mohamed Idris <ymohamed@lab1r4s11.42abudhabi.ae>
2023-06-06 07:56:11 +0400

    up

--- 0x08-python-more_classes/1-main.py
+++ 0x08-python-more_classes/1-main.py
@@ -2,9 +2,10 @@
 Rectangle = __import__('1-rectangle').Rectangle
 
 my_rectangle = Rectangle(2, 4)
-print(my_rectangle.__dict__)
+type(my_rectangle)
+# print(my_rectangle.__dict__)
 
-my_rectangle.width = 10
-my_rectangle.height = 3
-print(my_rectangle.height)
-print(my_rectangle.__dict__)
+# my_rectangle.width = 10
+# my_rectangle.height = 3
+# print(my_rectangle.height)
+# print(my_rectangle.__dict__)
