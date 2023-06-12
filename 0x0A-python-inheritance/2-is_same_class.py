e3a83d0a4eb31a05a52335fcc395475f64b28e23
Yassein Eltayeb Mohamed Idris <ymohamed@lab1r4s11.42abudhabi.ae>
2023-06-12 18:26:27 +0400

    up

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""this document has one class"""
+
+
+def is_same_class(obj, a_class):
+    """this is comparing class"""
+    if type(obj) is a_class:
+        return True
+    else:
+        return False
