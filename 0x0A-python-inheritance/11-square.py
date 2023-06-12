b3975364d69613ef8b5eef45b187492cba189b61
Yassein Eltayeb Mohamed Idris <ymohamed@lab1r4s11.42abudhabi.ae>
2023-06-12 22:37:36 +0400

    up

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+"""this is for class"""
+
+
+class BaseGeometry():
+    """a class with one method"""
+    def integer_validator(self, name, value):
+        if type(value) is not int or not isinstance(value, int):
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
+
+    def area(self):
+        raise Exception("area() is not implemented")
+
+
+class Rectangle(BaseGeometry):
+    """first one I write to inherit"""
+    def __init__(self, width, height):
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.__width = width
+        self.__height = height
+
+    def area(self):
+        return self.__height * self.__width
+
+    def __str__(self):
+        return f"[Rectangle] {self.__width}/{self.__height}"
+
+
+class Square(Rectangle):
+    """this is sub sub"""
+    def __init__(self, size):
+        self.integer_validator("size", size)
+        self.__size = size
+        super().__init__(self.__size, self.__size)
+
+    def __str__(self):
+	    return f"[Square] {self.__size}/{self.__size}"
