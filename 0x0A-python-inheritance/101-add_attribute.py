5042219d3eafbe2f83c97971b0d1fdec9298564b
Yassein Eltayeb Mohamed Idris <ymohamed@lab1r4s11.42abudhabi.ae>
2023-06-12 23:09:52 +0400

    up

--- 0x0A-python-inheritance/101-add_attribute.py
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
-
+"""last in bonus"""
 
 def add_attribute(obj, attr_name, attr_value):
+    """try attributes"""
     if not hasattr(obj, '__dict__'):
         raise TypeError("can't add new attribute")
     setattr(obj, attr_name, attr_value)
