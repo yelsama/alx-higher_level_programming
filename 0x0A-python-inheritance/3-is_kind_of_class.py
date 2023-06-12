88d5b6adc5bc009983af7e48baf3375f90bee2d7
Yassein Eltayeb Mohamed Idris <ymohamed@lab1r4s11.42abudhabi.ae>
2023-06-12 19:34:52 +0400

    up

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""this to comment all the file"""
+
+def is_kind_of_class(obj, a_class):
+    """this to describe this class"""
+    if isinstance(obj, a_class):
+        return True
+    else:
+        return False
