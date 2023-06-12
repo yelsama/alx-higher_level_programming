350714baf6218696d021dedc9736a2809ed69945
Yassein Eltayeb Mohamed Idris <ymohamed@lab1r4s11.42abudhabi.ae>
2023-06-12 20:58:54 +0400

    up

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -13,6 +13,7 @@ class BaseGeometry:
         if value <= 0:
             raise ValueError("{} must be greater than 0".format(name))
 
+
 class Rectangle(BaseGeometry):
     """first one I write to inherit"""
     def __init__(self, width, height):
