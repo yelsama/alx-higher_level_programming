a81623366ccd07df6d1a4a4b97cca1b3ae77b9c5
Yassein Eltayeb Mohamed Idris <ymohamed@lab1r4s11.42abudhabi.ae>
2023-06-12 22:15:29 +0400

    up

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -30,6 +30,7 @@ class Rectangle(BaseGeometry):
 
 
 class Square(Rectangle):
+    """this is sub sub"""
     def __init__(self, size):
         self.integer_validator("size", size)
         self.__size = size
