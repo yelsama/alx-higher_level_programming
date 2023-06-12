2dcf352a91d3f4d6fea35e4fae021a4eb9e6fc59
Yassein Eltayeb Mohamed Idris <ymohamed@lab1r4s11.42abudhabi.ae>
2023-06-12 22:01:04 +0400

    up

--- 0x0A-python-inheritance/9-rectangle.py
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -21,7 +21,9 @@ class Rectangle(BaseGeometry):
         self.integer_validator("height", height)
         self.__width = width
         self.__height = height
+
     def area(self):
         return self.__height * self.__width
+
     def __str__(self):
         return f"[Rectangle] {self.__width}/{self.__height}"
