c3da6c85123aa313cae1cae2c57375eae54fdece
Yassein Eltayeb Mohamed Idris <ymohamed@lab1r4s11.42abudhabi.ae>
2023-06-12 21:07:03 +0400

    up

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -18,6 +18,6 @@ class Rectangle(BaseGeometry):
     """first one I write to inherit"""
     def __init__(self, width, height):
         self.integer_validator("width", width)
-        self.integer_validator("height", height)
         self.__width = width
+        self.integer_validator("height", height)
         self.__height = height
