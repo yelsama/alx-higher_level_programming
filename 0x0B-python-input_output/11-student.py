481fa88b9988449aa28f20355bfa464ce2bb2c05
Yassein Eltayeb Mohamed Idris <ymohamed@lab1r4s11.42abudhabi.ae>
2023-06-14 00:44:26 +0400

    up

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+""" Student module
+"""
+
+
+class Student:
+    """ Student
+    """
+
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        if attrs is None:
+            return vars(self)
+        new_dic = {}
+        for key, value in self.__dict__.items():
+            if key in attrs:
+                new_dic[key] = value
+        return new_dic
+
+    def reload_from_json(self, json):
+        for jkey, jval in json.items():
+            setattr(self, jkey, jval)
