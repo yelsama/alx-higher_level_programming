41cccb29e84ad34462d18fd5b6855931218ec4da
Yassein Eltayeb Mohamed Idris <ymohamed@lab1r4s11.42abudhabi.ae>
2023-06-13 22:34:36 +0400

    up

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -5,5 +5,5 @@ import json
 
 def save_to_json_file(my_obj, filename):
     """converts my_obj to json representation"""
-    with open(filename, "a") as f:
+    with open(filename, "w") as f:
         f.write(json.dumps(my_obj))
--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,8 +1,17 @@
 #!/usr/bin/python3
 import sys
+import os
+
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
-i = 1
-while i in range(len(sys.argv)):
-   save_to_json_file() 
+file_name = "add_item.json"
+json_list = []
+
+if os.path.exists(file_name):
+    json_list = load_from_json_file(file_name)
+
+for i in sys.argv[1:]:
+    json_list.append(i)
+
+save_to_json_file(json_list, file_name)
