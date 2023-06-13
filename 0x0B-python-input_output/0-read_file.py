f7e70cf96f407f6d041b6760f48280ac42b87e15
Yassein Eltayeb Mohamed Idris <ymohamed@lab1r4s11.42abudhabi.ae>
2023-06-13 19:56:29 +0400

    up

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""doing reading"""
+
+
+def read_file(filename=""):
+    """to open and read filename"""
+    with open(filename) as f:
+        for line in f:
+            print(line, end="")
+    print()
