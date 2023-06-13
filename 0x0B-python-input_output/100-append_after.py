817fe1e17d5b879b5a42d0cdec7d81a4b6dbd6c6
Yassein Eltayeb Mohamed Idris <ymohamed@lab1r4s11.42abudhabi.ae>
2023-06-14 00:57:44 +0400

    up

---
+++ 0x0B-python-input_output/100-append_after.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""'''append_after'''"""
+
+
+def append_after(filename="", search_string="", new_string=""):
+	"""'''search and update'''"""
+	read = []
+	with open(filename, "r", encoding="utf-8") as f:
+		read = f.read()
+		i = 0
+
+		while i < len(read):
+			if search_string in read[i]:
+				read[i:i + 1] = [read[i], new_string]
+				i += 1
+			i += 1
+
+	with open(filename, "w", encoding="utf-8") as f:
+		f.writelines(read)
