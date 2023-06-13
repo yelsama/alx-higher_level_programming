0aee5f588db7967a9146aa1b5fbe096b62fd498c
Yassein Eltayeb Mohamed Idris <ymohamed@lab1r4s11.42abudhabi.ae>
2023-06-13 20:06:30 +0400

    up

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -5,5 +5,4 @@
 def write_file(filename="", text=""):
     """to open and read filename"""
     with open(filename, 'w', encoding='utf-8') as f:
-        n = f.write(text)
-    return n
+        return f.write(text)
