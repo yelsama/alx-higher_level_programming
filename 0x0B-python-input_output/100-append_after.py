39176d6f16ce2757d58794301f00727c4d899878
Yassein Eltayeb Mohamed Idris <ymohamed@lab1r4s11.42abudhabi.ae>
2023-06-14 01:02:34 +0400

    up

--- 0x0B-python-input_output/100-append_after.py
+++ 0x0B-python-input_output/100-append_after.py
@@ -3,17 +3,15 @@
 
 
 def append_after(filename="", search_string="", new_string=""):
-	"""search and update"""
-	read = []
-	with open(filename, "r", encoding="utf-8") as f:
-		read = f.readline()
-		i = 0
-
-		while i < len(read):
-			if search_string in read[i]:
-				read[i:i + 1] = [read[i], new_string]
-				i += 1
-			i += 1
-
-	with open(filename, "w", encoding="utf-8") as fw:
-		fw.writelines(read)
+    """search and update"""
+    read = []
+    with open(filename, "r", encoding="utf-8") as f:
+        read = f.readline()
+        i = 0
+        while i < len(read):
+            if search_string in read[i]:
+                read[i:i + 1] = [read[i], new_string]
+                i += 1
+            i += 1
+    with open(filename, "w", encoding="utf-8") as fw:
+        fw.writelines(read)
