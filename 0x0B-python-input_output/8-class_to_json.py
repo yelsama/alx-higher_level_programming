4129c83263e639aad42e0868ba5b22846f5bc40d
Yassein Eltayeb Mohamed Idris <ymohamed@lab1r4s11.42abudhabi.ae>
2023-06-13 23:06:15 +0400

    up

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
-"""convert string to json"""
+"""convert object to dictionary"""
 import json
 
 
 def class_to_json(obj):
-    """converts my_obj to json representation"""
+    """converts obj to json representation as dictionary"""
     return vars(obj)
