30686081e005ecf2328374ed14b0d064b0b37fa9
Yassein Eltayeb Mohamed Idris <ymohamed@lab1r4s11.42abudhabi.ae>
2023-06-20 20:47:15 +0400

    up

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -1,3 +1,12 @@
 #!/usr/bin/python3
 '''Module for Base unit tests.'''
 import unittest
+from models.base import Base
+
+
+class TestBAse(unittest.TestCase):
+    """to test base class"""
+    def test_id(self):
+        """check id"""
+        b1 = Base()
+        self.assertIsNotNone(b1.id)
