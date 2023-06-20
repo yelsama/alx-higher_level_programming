a22129d62d73ba4006ec3f08c226f02ed4d67ade
Yassein Eltayeb Mohamed Idris <ymohamed@lab1r4s11.42abudhabi.ae>
2023-06-20 20:59:35 +0400

    up

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -8,10 +8,12 @@ class TestBAse(unittest.TestCase):
     """to test base class"""
     def test_id(self):
         """check id"""
+        Base._Base__nb_objects = 0
         b1 = Base()
         self.assertIsNotNone(b1.id)
 
     def test_id2(self):
         """check id"""
+        Base._Base__nb_objects = 0
         b2 = Base(12)
         self.assertEqual(12, b2.id)
