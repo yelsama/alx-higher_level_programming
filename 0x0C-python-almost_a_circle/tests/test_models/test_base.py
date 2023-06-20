6672c80969368b23890ac0d7026359bd8842113d
Yassein Eltayeb Mohamed Idris <ymohamed@lab1r4s11.42abudhabi.ae>
2023-06-20 21:04:16 +0400

    up

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -18,6 +18,8 @@ class TestBAse(unittest.TestCase):
         b2 = Base(12)
         self.assertEqual(12, b2.id)
 
-    def test_type(self):
-        b = Base()
-        self.assertIsInstance(b, Base)
+    def test_init(self):
+        """check instance"""
+        Base._Base__nb_objects = 0
+        b2 = Base()
+        self.assertIsInstance(b2, Base)
