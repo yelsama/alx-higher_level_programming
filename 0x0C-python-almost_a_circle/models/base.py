29dd8484e771883ffb17bb625714d68261474e64
Yassein Eltayeb Mohamed Idris <ymohamed@lab1r4s11.42abudhabi.ae>
2023-06-24 01:39:13 +0400

    up

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 """Base class as requested"""
+import json
 
 
 class Base:
@@ -12,3 +13,7 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    def to_json_string(list_dictionaries):
+        """check the name and code"""
+        return json.dumps(list_dictionaries)
