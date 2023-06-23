1d2e718b2041cec219779862db1aadd869b73439
Yassein Eltayeb Mohamed Idris <ymohamed@lab1r4s11.42abudhabi.ae>
2023-06-23 20:37:56 +0400

    up

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -8,7 +8,7 @@ class Square(Rectangle):
     def __init__(self, size, x=0, y=0, id=None):
         """Initialize the constructor"""
         super().__init__(size, size, x, y, id)
-        
+ 
     def __str__(self):
 	    return "[{}] ({}) {}/{} - {}".format(
             type(self).__name__, self.id, self.x, self.y, self.width)
