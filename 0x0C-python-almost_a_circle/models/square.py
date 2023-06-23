d49035219be66a7f4647b3a801509351f87acd47
Yassein Eltayeb Mohamed Idris <ymohamed@lab1r4s11.42abudhabi.ae>
2023-06-23 20:51:54 +0400

    up

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -13,3 +13,12 @@ class Square(Rectangle):
         """overites the string for rectangle"""
         return "[{}] ({}) {}/{} - {}".format(
             type(self).__name__, self.id, self.x, self.y, self.width)
+
+    @property
+    def size(self):
+        return self.width
+
+    @size.setter
+    def size(self, size):
+        self.width = size
+        self.height = size
