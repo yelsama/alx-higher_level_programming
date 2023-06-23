ac7c134f9c15db0463da4af46c17dda4cae03bed
Yassein Eltayeb Mohamed Idris <ymohamed@lab1r4s11.42abudhabi.ae>
2023-06-24 01:42:35 +0400

    up

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -16,4 +16,6 @@ class Base:
 
     def to_json_string(list_dictionaries):
         """check the name and code"""
+        if list_dictionaries is None or len(list_dictionaries) == 0:
+            return []
         return json.dumps(list_dictionaries)
