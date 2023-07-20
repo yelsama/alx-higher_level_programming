936b73434ea021f815fd900743bb8de6f599f03c
Yassein Eltayeb Mohamed Idris <ymohamed@lab3r4s5.42abudhabi.ae>
2023-07-21 00:14:04 +0400

    up

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,4 +1,4 @@
--- Creates the database `hbtn_0d_2` and
+-- Creates the database `hbtn_0d_2` where
 -- the user `user_0d_2` with specific privileges.
 CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
 DROP USER IF EXISTS 'user_0d_2'@'localhost';
