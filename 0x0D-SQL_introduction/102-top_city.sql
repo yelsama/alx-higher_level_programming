a9df40b2606432494f348c4a408b2c04f74f4336
Yassein Eltayeb Mohamed Idris <ymohamed@lab3r4s5.42abudhabi.ae>
2023-07-20 19:15:26 +0400

    up

---
+++ 0x0D-SQL_introduction/102-top_city.sql
@@ -0,0 +1,2 @@
+-- displays the max temperature of each state (ordered by State name)
+SELECT city, AVG(value) AS avg_temp FROM temperatures WHERE month IN (7, 8) GROUP BY city ORDER BY avg_temp DESC LIMIT 3;
