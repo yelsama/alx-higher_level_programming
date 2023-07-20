4fbd7110d1a1195385fab664d757b8aba34e187a
Yassein Eltayeb Mohamed Idris <ymohamed@lab3r4s5.42abudhabi.ae>
2023-07-20 13:57:55 +0400

    up

--- 0x0D-SQL_introduction/1-create_database_if_missing.sql
+++ 0x0D-SQL_introduction/1-create_database_if_missing.sql
@@ -1,2 +1,2 @@
 -- to creat database if not existing
-CREATE DATABASE IF NOT EXISTS hbtn_0c_0
+CREATE DATABASE IF NOT EXISTS hbtn_0c_0;
---
+++ 0x0D-SQL_introduction/2-remove_database.sql
@@ -0,0 +1,2 @@
+-- removes database if exists
+REMOVE DATABASE IF EXISTS hbtn_0c_0;
