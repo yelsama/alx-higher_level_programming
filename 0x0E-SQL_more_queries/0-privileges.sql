b65f9ea10f1c8d988cadb33fa64fa8095024ea31
Yassein Eltayeb Mohamed Idris <ymohamed@lab3r4s5.42abudhabi.ae>
2023-07-20 23:40:42 +0400

    up

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,4 +1,4 @@
--- Script lists all privileges of the MySQL users 
+-- Script lists privileges of users 
 -- user_0d_1 and user_0d_2 on localhost.
 SHOW GRANTS FOR 'user_0d_1'@'localhost';
 SHOW GRANTS FOR 'user_0d_2'@'localhost';
