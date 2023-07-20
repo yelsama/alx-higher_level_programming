422130c340bb939a50867653ee7f7d540ecfc0b3
Yassein Eltayeb Mohamed Idris <ymohamed@lab3r4s5.42abudhabi.ae>
2023-07-20 14:44:10 +0400

    up

--- 0x0D-SQL_introduction/4-first_table.sql
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -1,2 +1,2 @@
 -- create a table with id of type int and name of type varchar(256)
-CREATE DATABASE IF NOT EXISTS first_table (id INT, name VARCHAR(256));
+CREATE TABLE IF NOT EXISTS first_table (id INT, name VARCHAR(256));
