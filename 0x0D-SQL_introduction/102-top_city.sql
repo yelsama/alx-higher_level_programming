7d4dec007e6e7e4199b4c1e7584ac18642a3d84b
Yassein Eltayeb Mohamed Idris <ymohamed@lab3r4s5.42abudhabi.ae>
2023-07-20 19:16:11 +0400

    up

--- 0x0D-SQL_introduction/102-top_city.sql
+++ 0x0D-SQL_introduction/102-top_city.sql
@@ -1,2 +1,2 @@
--- displays the max temperature of each state (ordered by State name)
+-- display the top 3 of cities temperature during July and August ordered by temperature (descending)
 SELECT city, AVG(value) AS avg_temp FROM temperatures WHERE month IN (7, 8) GROUP BY city ORDER BY avg_temp DESC LIMIT 3;
