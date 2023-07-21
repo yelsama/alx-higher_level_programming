cb4b548510ba69bca60597c9749b2306ac3888b2
Yassein Eltayeb Mohamed Idris <ymohamed@lab3r4s5.42abudhabi.ae>
2023-07-21 18:55:15 +0400

    up

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,2 +1,2 @@
 -- script that lists all the cities of California that can be found in the database hbtn_0d_usa.
-FROM hbtn_0d_usa SHOW TABLE WHERE name="California";
+FROM cities SELECT id, name WHERE state_id=(FROM states SELECT id WHERE name='Clifornia');
