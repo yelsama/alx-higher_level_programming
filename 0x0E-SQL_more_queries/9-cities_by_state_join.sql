86eff32466658d1c88fb2f11f1f614b8469db025
Yassein Eltayeb Mohamed Idris <ymohamed@lab3r4s5.42abudhabi.ae>
2023-07-21 19:15:20 +0400

    up

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,4 +1,5 @@
 -- list from two tables
 -- second line
 SELECT cities.id, cities.name, states.name FROM cities JOIN states
-	ON cities.state_id = states.id;
+	ON cities.state_id = states.id
+	ORDER BY cities.id ASC;
